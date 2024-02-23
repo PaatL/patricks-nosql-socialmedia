9b72ccd6e0038dc336352724a18ba66ca3866a24
Patrick Li <lipatrick093@gmail.com>
2024-02-23 13:38:39 -0500

    test

---
+++ models/Thought.js
@@ -0,0 +1,37 @@
+const {Schema,model} = require('mongoose')
+
+const thoughtSchema = new Schema(
+    {
+        thought: {
+            type: String,
+            required: true,
+            maxlength: 280,
+            minlength:1,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: Date().toString(),
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Reaction',
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+thoughtSchema.virtual('reactionCount').get(function(){
+    return this.reactions.length;
+});
